1e06e90b8dd1a5a328cf72db09666b0540c709b0
strimaitis <33992296+strimaitis@users.noreply.github.com>
2018-11-17 16:53:49 -0600

    Add files via upload

---
+++ Assets/DoorAnimationScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnimationScript : MonoBehaviour {
+
+	Animator anim;
+	BoxCollider coll;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		coll = GetComponent<BoxCollider> ();
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		anim.SetTrigger("Open_Trigger");
+		coll.isTrigger = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// OnTriggerEnter (coll);
+	}
+}
---
+++ Assets/LightScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour {
+
+	Vector3 myVector;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myVector = new Vector3 (1.0f, 1.0f, 1.0f);
+		while (true) {
+			transform.Rotate (myVector * Time.deltaTime);
+		}
+	}
+}
